381278a34c46d6586994d3325233c5503450a2de
Matthew Crouch <croucm01@mc-s083375.local>
2013-04-29 15:48:17 +0100

    Removed safari useragent from driver

--- lib/cucumber_salad/drivers/safari.rb
+++ lib/cucumber_salad/drivers/safari.rb
@@ -7,8 +7,7 @@ Capybara.register_driver :safari do |app|
   )
   Helper::Driver.register(
     :smartphone,
-    :poltergeist,
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.29.13 (KHTML, like Gecko) Version/6.0.4 Safari/536.29.13"
+    :poltergeist
   )
   driver
 end
